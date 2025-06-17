5a86f503ae7c66b916b28f80b3ea034fab2cc7de
Jacob Feldman <jfeldman777@gmail.com>
2025-06-17 16:36:04 +0300

    working1

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Базовый маршрут
+app.get('/', (req, res) => {
+  res.send('Сервер работает');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
